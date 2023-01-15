53d6ad1d9b5e3c047d8204e2d1d8eef786f9e41e
belovetech <belovetech@gmail.com>
2023-01-15 14:12:09 +0100

    [Feat]: create SafeDiv function that divides and return

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,6 +1,6 @@
 -- creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
 
-DROP FUNCTION SafeDiv IF EXISTS
+DROP FUNCTION IF EXISTS SafeDiv;
 
 DELIMITER $$
 
