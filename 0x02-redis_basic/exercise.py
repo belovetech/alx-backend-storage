80f66e463377b6fbc166765641b4b3e10e862750
belovetech <belovetech@gmail.com>
2023-01-18 21:37:22 +0100

    [Feat]: read from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Writing strings to Redis"""
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 import uuid
 
 
@@ -25,3 +25,23 @@ class Cache:
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """Convert key to the desired format"""
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, value: bytes) -> str:
+        """Get str from the cache"""
+        return str(value.decode('utf-8'))
+
+    def get_int(self, value: bytes) -> int:
+        """Get int from the cache"""
+        try:
+            value = int(value.decode('utf-8'))
+        except Exception:
+            value = 0
+        return value
